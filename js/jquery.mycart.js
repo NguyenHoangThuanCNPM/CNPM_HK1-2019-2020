9dc8e7ac8ebfb5be25cb452172928fde59a94cae
Khoa Thanh <thanhdaikhoa3289@gmail.com>
2019-12-09 18:13:35 +0700

    update Payment button and fix bugs
    Thanh Đại Khoa - 517H0008

--- js/jquery.mycart.js
+++ js/jquery.mycart.js
@@ -185,7 +185,7 @@
         '<table class="table table-hover table-responsive" id="' + idCartTable + '"></table>' +
         '</div>' +
         '<div class="modal-footer">' +
-        '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+        '<button type="button" class="btn btn-default"> <a href="login.html" style="text-decoration: none;">Payment</a> </button>' +
 		
         '</div>' +
         '</div>' +
